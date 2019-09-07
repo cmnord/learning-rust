bc3ac9467ef4e890bf5e98776807619b51309032
Claire Nord <cnord@mit.edu>
2019-09-07 16:03:39 -0400

    sipb: implement clone, add count

--- src/sipb/game.rs
+++ src/sipb/game.rs
@@ -1,53 +1,57 @@
-struct PlayingState;
+struct PlayingState(i8);
 
 impl Drop for PlayingState {
-	fn drop(&mut self) {
-		println!("Dropping a Playing")
-	}
+    fn drop(&mut self) {
+        println!("Dropping a Playing")
+    }
 }
 
-struct WonState;
-struct LostState;
+impl Clone for PlayingState {
+    fn clone(&self) -> Self {
+        PlayingState(self.0 + 1)
+    }
+}
 
-fn win_game(_p: PlayingState) -> WonState {
-    WonState
+struct WonState(i8);
+struct LostState(i8);
+
+fn win_game(p: PlayingState) -> WonState {
+    WonState(p.0)
 }
 
 // _prefix for unused variable
-fn lose_game(_p: PlayingState) -> LostState {
-    LostState
+fn lose_game(p: PlayingState) -> LostState {
+    LostState(p.0)
 }
 
 fn print_won(w: &WonState) -> &WonState {
-    println!("Won!");
+    println!("savepoint {} won!", w.0);
     &w
 }
 
 fn print_lost(l: &LostState) -> &LostState {
-    println!("Lost!");
+    println!("savepoint {} lost!", l.0);
     &l
 }
 
-
 fn main() {
-	// lose_game(32); 			// this should be a compile-time error
+    // lose_game(32); 			// this should be a compile-time error
 
-	println!("game 0 starting");
-	let game0 = PlayingState;
-	let won0 = win_game(game0);
-	// print_lost(won0);			// This should be a compile-time error
-	print_won(&won0);
-	print_won(&won0);
+    println!("game 0 starting");
+    let game0 = PlayingState(0);
+    let game0_1 = game0.clone();
+    let won0 = win_game(game0);
+    // print_lost(won0);			// This should be a compile-time error
+    print_won(&won0);
+    print_won(&won0);
 
-	// lose_game(game0);   		// This should be a compile-time error
-	// lose_game(won0);   		// This should be a compile-time error
-	// let gameA = game0;			// This should be a compile-time error
-	// lose_game(gameA); 		// This should be a compile-time error
+    // lose_game(game0);   		// This should be a compile-time error
+    // lose_game(won0);   		// This should be a compile-time error
+    // let gameA = game0;			// This should be a compile-time error
+    // lose_game(gameA); 		// This should be a compile-time error
 
-	println!("game 1 starting");
-	let game1 = PlayingState;
-	let lost1 = lose_game(game1);
-	print_lost(&lost1);
-	print_lost(&lost1);
-	
+    println!("game 1 starting");
+    let lost1 = lose_game(game0_1);
+    print_lost(&lost1);
+    print_lost(&lost1);
 }

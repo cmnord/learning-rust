5e5371329837a240b5049a11662749808cd0804e
Claire Nord <cnord@mit.edu>
2019-09-08 13:50:13 -0400

    sipb rollup: fix list
    I have no idea what happened here

--- src/sipb/rollup.rs
+++ src/sipb/rollup.rs
@@ -1,6 +1,6 @@
-struct List {
+struct List<'t> {
     value: i32,
-    next: Option<List>,
+    next: Option<&'t List<'t>>,
 }
 
 fn print_list(list: &Option<&List>) {
@@ -8,19 +8,19 @@ fn print_list(list: &Option<&List>) {
         None => {}
         Some(list) => {
             println!("{}", list.value);
-            print_list(list.next);
+            print_list(&list.next);
         }
     }
 }
 
-fn build_and_print_list(iter: &std::slice::Iter<i32>, tail: Option<&List>) {
+fn build_and_print_list(iter: &mut std::slice::Iter<i32>, tail: Option<&List>) {
     match iter.next() {
         None => print_list(&tail),
         Some(value) => {
             build_and_print_list(
                 iter,
-                Some(List {
-                    value: value,
+                Some(&List {
+                    value: *value,
                     next: tail,
                 }),
             );
@@ -30,6 +30,6 @@ fn build_and_print_list(iter: &std::slice::Iter<i32>, tail: Option<&List>) {
 
 fn main() {
     let values = [5, 23, 0, 17, 6, 7, 10, 3, 1];
-    let  iter = values.iter();
-    build_and_print_list(&iter, None);
+    let mut iter = values.iter();
+    build_and_print_list(&mut iter, None);
 }
